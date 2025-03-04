535449bef9517e42d250d724009efd678236b028
teressaep <teressa_ep@gmx.com>
2025-03-04 21:30:47 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,9 @@
+// This is a comment in C++
+
+#include <iostream> // This is a header file
+using namespace std; // This is a statement to use the standard library
+
+int main() {
+  cout << "Hello World!" << endl; // This prints "Hello World!" to the console
+  return 0; // This ends the program with a return value of 0
+}
